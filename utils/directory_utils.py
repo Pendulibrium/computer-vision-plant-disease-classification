1fe0bd8a782c0338e7bdadc5c796d839c862a052
Simona Ivanova <simona.ivanova@webfactory.mk>
2018-03-16 15:40:19 +0100

    Add method for generating images with different background using segmented images

--- utils/directory_utils.py
+++ utils/directory_utils.py
@@ -1,4 +1,5 @@
 from glob import glob
+import numpy as np
 import os
 import h5py
 import cv2
@@ -44,3 +45,46 @@ def split_data(images_directory):
             folder_index += 1
 
 #split_data("/Users/wf-markosmilevski/computer-vision-plant-disease-classification/data/color")
+
+
+def generate_different_background_images(backgrounds_path="../data/backgrounds/",
+                                         images_directory="../data/segmented/"):
+    category_names = [name for name in os.listdir(images_directory)]
+    background_image_files = get_file_names_from_directory(directory_path=backgrounds_path, extensions=["*.jpg"])
+
+    num_background_filters = len(background_image_files[0])
+    for i in range(num_background_filters):
+        current_background = cv2.imread(background_image_files[i])[:256, :256, :]
+        background_image_name_dir = background_image_files[i].rsplit( "/", 1 )[ -1 ].rsplit(".", 1)[0]
+
+        path = '../data/' + background_image_name_dir
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+        for category in category_names:
+            category_path = images_directory + "/" + category
+            images_in_category_ = get_file_names_from_directory(directory_path=category_path,
+                                                                extensions=["*.JPG", "*.jpg"])
+            images_in_category = images_in_category_[0] + images_in_category_[1]
+            num_images = len(images_in_category)
+
+            image_save_path = path + '/' + category
+
+            if not os.path.exists(image_save_path):
+                os.makedirs(image_save_path)
+
+            for i in range(num_images):
+                current_image = cv2.imread(images_in_category[i])
+                current_image = cv2.resize(current_image, (256, 256))
+
+                current_image_name = images_in_category[i].rsplit("/", 1)[-1]
+                current_inverted = invert_image(current_image)
+
+                transformed_image = current_inverted * current_background + current_image
+                cv2.imwrite(image_save_path + '/' + current_image_name, transformed_image)
+
+def invert_image(image):
+    image_copy = image.copy()
+    image_copy[np.where((image_copy == [0, 0, 0]).all(axis=2))] = 1
+    image_copy[np.where((image_copy != [1, 1, 1]).all(axis=2))] = 0
+    return image_copy
